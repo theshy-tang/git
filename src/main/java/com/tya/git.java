c8a04e2948f8f84f7bf345da0410ccef76f3e1c8
theshy-tang <2046272105@qq.com>
2023-03-04 21:29:02 +0800

    push test

--- src/main/java/com/tya/git.java
+++ src/main/java/com/tya/git.java
@@ -13,5 +13,6 @@ public class git {
         System.out.println("aaaaaaaaa3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
