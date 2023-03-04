3d9c8a18ed733704c79e254171c1fda593b164e4
theshy-tang <2046272105@qq.com>
2023-03-04 20:17:29 +0800

    first test

---
+++ src/main/java/com/tya/git.java
@@ -0,0 +1,13 @@
+package com.tya;
+
+/**
+ * <h4>git</h4>
+ *
+ * @author : 唐永澳
+ * @date : 2023-03-04 20:13
+ **/
+public class git {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+    }
+}
